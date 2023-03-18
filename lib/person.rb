8ced3f67640cdb2a98cf3d8785a93619550aedb1
Nelofar Zabi <mutau85@gmail.com>
2023-03-18 21:35:17 +0200

    modify the code in book, person & student

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,34 @@
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
+require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
 
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def correct_name
+    @name
+  end
+
+  def of_age?
+    @age >= 18
   end
 
   def can_use_services?
     of_age? || @parent_permission
   end
 
-  private
-
-  def of_age?
-    @age >= 18
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
+
+  attr_reader :id, :rentals
+  attr_accessor :name, :age
+  private :of_age?
 end
